580aff41dfa794aeb1cd107dea2aa46664204c6f
Matthew Campbell <hyper@hyperworks.nu>
2022-10-11 19:45:15 +0700

    add song select and a music visualizer

--- scenes/SongSelect.gd
+++ scenes/SongSelect.gd
@@ -1,5 +1,5 @@
 extends Node2D
-
+@onready var item_list = get_node(^"ItemList")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,3 +13,18 @@ func _process(delta):
 
 func _on_back_btn_pressed():
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
+
+
+func _on_select_btn_pressed():
+	var selected = item_list.get_selected_items()
+	if len(selected) < 1:
+		return
+
+	var selected_song = item_list.get_item_text(selected[0])
+	var selected_song_id = item_list.get_index(selected[0])
+
+	print(selected_song)
+	print(selected_song_id)
+	PlayerVariables.current_song = selected_song
+
+	get_tree().change_scene_to_file("res://scenes/song.tscn")
