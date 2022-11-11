a1c25d3e21c187b73676ffb4252baf58c28de5bc
Robert-Jan Zandvoort <robbertzzz@gmail.com>
2022-11-11 12:11:51 +0000

    Basic microphone input, doesn't work too great yet but we'll probably endlessly tweak this.

--- autoload/instrument_input/microphone_input.gd
+++ autoload/instrument_input/microphone_input.gd
@@ -18,14 +18,24 @@ func _process(delta):
 	var volume: float
 	
 	var threshold = -40
+	var pitch_accuracy: float = 0.5
 	
+	var previous_inputs = inputs.duplicate()
 	inputs = []
 	for note in NoteFrequency.CHROMATIC:
-		min_frequency = 0.5 * note * pow(2, 23/24.0)
-		max_frequency = note * pow(2, 1/24.0)
+		min_frequency = 0.5 * note * pow(2, (23 + pitch_accuracy)/24.0)
+		max_frequency = note * pow(2, (1 - pitch_accuracy)/24.0)
 		volume = linear_to_db(spectrum_analyzer.get_magnitude_for_frequency_range(min_frequency, max_frequency).x)
 		if volume >= threshold:
 			inputs.append(note)
+	
+	for note in previous_inputs:
+		if not note in inputs:
+			note_ended.emit(note)
+	
+	for note in inputs:
+		if not note in previous_inputs:
+			note_started.emit(note)
 
 
 func get_inputs()->Array:
