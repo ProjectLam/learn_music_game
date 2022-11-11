a6384a117cb49c450bd29e1a5b5433cbf70cf0ca
Matthew Campbell <hyper@hyperworks.nu>
2022-11-11 17:02:46 +0700

    First pass search directories for music, in next PR parse xml (#5)
    * parse v1
    
    * tweak

---
+++ scenes/song_parser/song.gd
@@ -0,0 +1,28 @@
+extends Node
+class_name Song
+
+@export var artistName = ""
+@export var title = ""
+@export var artistNameSort = ""
+@export var artistYear = ""
+@export var artistXXXXYear = ""
+@export var bobXXXX = ""
+@export var ebeats:Ebeats = Ebeats.new()
+@export var songMusicFile = ""
+
+#  <albumName>The name</albumName>
+#  <albumNameSort>name, the</albumNameSort>
+#  <albumYear>1981</albumYear>
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func parser_ebeats():
+	print("weeee ebeats")
