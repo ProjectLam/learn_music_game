f18b13986749682e2b1c21ff4c0e9b6a0bdf80fb
Robert-Jan Zandvoort <robbertzzz@gmail.com>
2022-11-11 12:27:03 +0000

    Player input (#6)
    * Added a utility script for working with note pitches
    
    * Added code for different instruments
    
    * Implemented MIDI input, needs some more testing.
    
    * Removed print from midi input after confirming it's working. Added logic for microphone input, which might need to be tweaked.
    
    * Basic microphone input, doesn't work too great yet but we'll probably endlessly tweak this.

---
+++ autoload/instrument_input/midi_input.gd
@@ -0,0 +1,36 @@
+extends InputInstrument
+
+
+const MIDI_OFFSET = -21
+
+var notes: PackedByteArray
+
+
+func _ready():
+	OS.open_midi_inputs()
+	
+	notes.resize(NoteFrequency.CHROMATIC.size())
+	notes.fill(0)
+
+
+func get_inputs()->Array:
+	var inputs = []
+	
+	for i in notes.size():
+		if notes[i] == 1:
+			inputs.append(NoteFrequency.CHROMATIC[i])
+	
+	return inputs
+
+
+func _input(event):
+	if event is InputEventMIDI and (event.message == MIDI_MESSAGE_NOTE_ON or event.message == MIDI_MESSAGE_NOTE_OFF):
+		var chromatic_index = event.pitch + MIDI_OFFSET
+		if chromatic_index < 0 or chromatic_index >= NoteFrequency.CHROMATIC.size():
+			return
+		if event.message == MIDI_MESSAGE_NOTE_ON and notes[chromatic_index] != 1:
+			notes[chromatic_index] = 1
+			note_started.emit(NoteFrequency.CHROMATIC[event.pitch + MIDI_OFFSET])
+		elif event.message == MIDI_MESSAGE_NOTE_OFF and notes[chromatic_index] != 0:
+			notes[chromatic_index] = 0
+			note_ended.emit(NoteFrequency.CHROMATIC[event.pitch + MIDI_OFFSET])
