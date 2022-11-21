356a2bd2762feb2cfe38b38343e149b0f62311db
Oğuzhan Eroğlu <rohanrhu2@gmail.com>
2022-11-21 15:11:13 +0300

    Item circles and improvements

--- scenes/InstrumentSelection/InstrumentSelectionItem.gd
+++ scenes/InstrumentSelection/InstrumentSelectionItem.gd
@@ -9,7 +9,19 @@ func _ready():
 	pass
 
 func _process(delta):
-	pass
+	var x = get_viewport().get_mouse_position().x / get_viewport_rect().size.x
+	var y = get_viewport().get_mouse_position().y / get_viewport_rect().size.y
+	var pos = Vector2(x, y)
+	
+	$Circle.material = $Circle.material.duplicate()
+	
+	$Circle.material.set_shader_parameter("lighting_point", pos)
 
 func set_item(p_item: TInstrumentSelectionItem):
 	item = p_item
+
+func _on_mouse_entered():
+	$Circle.material.set_shader_parameter("indicate", true)
+
+func _on_mouse_exited():
+	$Circle.material.set_shader_parameter("indicate", false)
