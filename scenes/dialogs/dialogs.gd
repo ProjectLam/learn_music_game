b149acbfda50dcdc245b5f625de328c737523c19
Oğuzhan Eroğlu <rohanrhu2@gmail.com>
2022-12-02 03:46:57 +0300

    Singular dialog open

---
+++ scenes/dialogs/dialogs.gd
@@ -0,0 +1,19 @@
+extends Control
+
+var DIALOGS = [
+	GLoginDialog,
+	GRegisterDialog
+]
+
+func _ready():
+	pass
+
+func _process(delta):
+	pass
+
+func open_single(dialog) -> void:
+	for other in DIALOGS:
+		if other != dialog:
+			other.close()
+	
+	dialog.open()
