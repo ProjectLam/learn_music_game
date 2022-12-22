43c727ba3fc863c60d1b061c2259ff83c60169e5
O??uzhan Ero??lu <rohanrhu2@gmail.com>
2022-12-22 09:37:25 +0300

    Add tuner button

--- scenes/instrument_menu/instrument_menu.gd
+++ scenes/instrument_menu/instrument_menu.gd
@@ -15,5 +15,8 @@ func _on_Matches_selected() -> void:
 func _on_Settings_selected() -> void:
 	get_tree().change_scene_to_file("res://scenes/song_selection/song_selection.tscn")
 
+func _on_Tuning_selected() -> void:
+	get_tree().change_scene_to_file("res://scenes/tuner.tscn")
+
 func _on_Quit_selected() -> void:
 	get_tree().change_scene_to_file("res://scenes/instrument_selection/instrument_selection.tscn")
