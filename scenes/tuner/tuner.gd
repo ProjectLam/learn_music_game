87d09231ab8a9ce5eb828e836635749e89748344
O??uzhan Ero??lu <rohanrhu2@gmail.com>
2022-12-29 23:08:00 +0300

    Note detection improvements

--- scenes/tuner/tuner.gd
+++ scenes/tuner/tuner.gd
@@ -2,11 +2,13 @@ extends Control
 
 var cNotesItem = preload("res://scenes/tuner/notes_item.tscn")
 
-@export var pitch_freq = 440
-@export var sub_steps = 9
-@export var energy_threshold = -60
+@export var pitch_freq: float = 440
+@export var sub_steps: int = 9
+@export var energy_threshold: float = -60
+@export var freq_start_div: float = 2.0
+@export var freq_end_div: float = 2.0
 
-@export var sensitivity_interpolation_delay = 20
+@export var sensitivity_interpolation_delay = 60
 var sensitivity_interpolation_time = 0
 
 @onready var nItems = find_child("Notes").find_child("Items")
@@ -61,8 +63,17 @@ func note_freq_range(i: int) -> Vector2:
 		return Vector2(26.5, 28.5)
 	
 	return Vector2(
-		note_freq(i) - (note_freq(i) - note_freq(i-1)) / 1.8,
-		note_freq(i) + (note_freq(i+1) - note_freq(i)) / 1.9
+		note_freq(i) - ((note_freq(i) - note_freq(i-1)) / freq_start_div),
+		note_freq(i) + ((note_freq(i+1) - note_freq(i)) / freq_end_div)
+	)
+
+func note_sub_freq_range(i: int) -> Vector2:
+	if i == 0:
+		return Vector2(26.5, 28.5)
+	
+	return Vector2(
+		note_freq(i-1),
+		note_freq(i+1)
 	)
 
 func _ready():
@@ -117,9 +128,9 @@ func _process(delta):
 		sub_max_energy = -INF
 		sub_res_freq = -INF
 		
-		sub_range = freq_end - freq_start
+		sub_range = note_sub_freq_range(b88_note_i).y - note_sub_freq_range(b88_note_i).x
 		sub_step_freq = sub_range / sub_steps
-		sub_range_start = freq_start
+		sub_range_start = note_sub_freq_range(b88_note_i).x - sub_step_freq
 		sub_freq_ratio = 0
 		
 		for j in sub_steps:
@@ -134,6 +145,13 @@ func _process(delta):
 				sub_freq_ratio = float(j) / sub_steps
 				sub_res_freq = sfreq
 				sub_max_energy = senergy
+				
+				if diff < sensitivity_interpolation_delay:
+					nonsensitive_sub_freq_total += sub_freq_ratio
+					nonsensitive_sub_freq_count += 1
+					if not nonsensitive_sub_freqs.has(str(sub_freq_ratio)):
+						nonsensitive_sub_freqs[str(sub_freq_ratio)] = 0
+					nonsensitive_sub_freqs[str(sub_freq_ratio)] += senergy
 		
 		b88_note_i = b88_note_i
 		var note_i = (69 + int(round(12 * log(res_freq / pitch_freq) / log(2)))) % 12
@@ -144,16 +162,10 @@ func _process(delta):
 			for k in nonsensitive_sub_freqs.keys():
 				var c = nonsensitive_sub_freqs[k]
 				if c > t:
-					nonsensitive_sub_freq_ratio = float(k)
+					nonsensitive_sub_freq_ratio = k
 				t = c
 			nonsensitive_sub_freqs = {}
 #			nonsensitive_sub_freq_ratio = float(nonsensitive_sub_freq_total) / nonsensitive_sub_freq_count
-		else:
-			nonsensitive_sub_freq_total += sub_freq_ratio
-			nonsensitive_sub_freq_count += 1
-			if not nonsensitive_sub_freqs.has(sub_freq_ratio):
-				nonsensitive_sub_freqs[sub_freq_ratio] = 0
-			nonsensitive_sub_freqs[sub_freq_ratio] += 1
 		
 		var note = mnemonics[note_i]
 		nFreqLabel.text = note
@@ -177,9 +189,11 @@ func _process(delta):
 		
 		tween.tween_property(nItems.get_child(note_i).find_child("MnemonicLabel").label_settings, "font_color", Color.WHITE, 0.25)
 		
-		var stick_angle = stick_start + (stick_range * nonsensitive_sub_freq_ratio)
+		var stick_angle = stick_start + (stick_range * float(nonsensitive_sub_freq_ratio))
 		var stick_radians = deg_to_rad(stick_angle)
 		tween.tween_property(nStick, "rotation", stick_radians, 0.25)
+		
+		print("nonsensitive_sub_freq_ratio: ", nonsensitive_sub_freq_ratio , " - ratio: ", sub_freq_ratio)
 	
 	if diff >= sensitivity_interpolation_delay:
 		sensitivity_interpolation_time = Time.get_ticks_msec()
